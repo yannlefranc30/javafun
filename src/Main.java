58f9d27bdca41172a4b6883fb9683afc762bf03d
user <yannlefranc330@gmail.com>
2023-09-22 21:45:30 +0200

    twerk

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        //declaration de deux variables
+        //code perss
+
+            String od = "tu twerk bien";
+            String odz = "bravo";
+
+
+            System.out.println(od);
+            System.out.println(odz);
+
+        }
+    }
